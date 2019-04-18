d7cef1c24e6c177b6779b705a4ae18d07a7c22ef
sarah <sarahkharraz@gmail.com>
2019-04-19 00:04:02 +0100

    Setup the logic of the buttons - Previous/Next

--- carousel.js
+++ carousel.js
@@ -4,13 +4,17 @@ imgCarousel[0] = "img/pic01.jpg";
 imgCarousel[1] = "img/pic02.jpg";
 imgCarousel[2] = "img/pic03.jpg";
 
+// Reeditable variables for the buttons logic
+let i = 0;
+let x = (imgCarousel.length) - 1;
+
 // Setup the elements of my HTML page
 const elements = {
   carousel: document.querySelector('.carousel'),
   btn: {
     left: document.querySelector('.btnLeft'),
     right: document.querySelector('.btnRight')
-  };
+  }
 };
 
 // Setup the event listeners for my buttons
@@ -21,4 +25,15 @@ function EventListeners() {
   elements.btn.right.onclick = function() {
     displayNext()
   };
+};
+
+// Create the buttons logic depending on the amount of images of the imgCarousel variable
+function displayPrevious() {
+  (i <= 0) ? i = 3 : i--;
+  changeImg(i);
+};
+
+function displayNext() {
+  (i >= x) ? i = 0 : i++;
+  changeImg(i);
 };
