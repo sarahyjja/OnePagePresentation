24387e1e7e5fdcaabf9b085f4493bec885ae8d82
sarah <sarahkharraz@gmail.com>
2019-04-24 13:27:33 +0100

    Refactoring and cleanning comments

--- carousel.js
+++ carousel.js
@@ -1,14 +1,10 @@
-// Create an eventListener for the whole file so it can be read at the top of HTML and stop using the last function controller
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Create an empty array variable to store my images and define their positions in the carousel
   const imgCarousel = ["./img/1.jpg", "./img/2.jpg", "./img/3.jpg", "./img/4.jpg", "./img/5.jpg"];
 
-  // Reeditable variables for the buttons logic
   let i = 0;
   let x = (imgCarousel.length) - 1;
 
-  // Setup the elements of my HTML page
   const elements = {
     carousel: document.querySelector('.carousel'),
     btn: {
@@ -17,7 +13,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   };
 
-  // Setup the eventListeners for my buttons and keyboard
   elements.btn.left.onclick = function() {
     displayPrevious()
   };
@@ -34,7 +29,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  // Setup rotation timing when no click
   let int = 3000;
   interval = setInterval(displayNext, int);
 
@@ -46,7 +40,6 @@ document.addEventListener('DOMContentLoaded', function() {
     clearInterval(interval);
   };
 
-  // Create the buttons logic depending on the amount of images of the imgCarousel variable
   function displayPrevious() {
     (i <= 0) ? i = 4 : i--;
     changeImg(i);
@@ -57,12 +50,10 @@ document.addEventListener('DOMContentLoaded', function() {
     changeImg(i);
   };
 
-  // Call of the hardcoded images and create a function who will be used to change them depending on index
   function changeImg() {
     elements.carousel.style.backgroundImage = 'url(' + imgCarousel[i] + ')';
   };
 
-  // Make the calls to display the carousel = controller
   let initialize = function() {
     changeImg(i);
   };
